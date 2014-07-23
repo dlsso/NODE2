28013e5647a5d30e31a9c59ccc04ce26f7ad5ab9
dlsso <supertonesfan@hotmail.com>
2014-07-22 20:05:59 -0600

    Did part 3

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+// Instructions for this exercise suggest that res.header/send should be moved
+// outside app.get, which of course doesn't work. There is also no solution
+// for this particular exercise on github.
+
+
 var fs = require('fs')
-var fileContents = fs.readFileSync('data.txt')
-// res.header('Content-Type', 'text/html')
-
-
-
 var express = require('express');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.header('Content-Type', 'text/html')
-	res.send(fileContents);
+
+	fs.readFile('data.txt', function (err, data){
+		res.header('Content-Type', 'text/html')
+		res.send(data);
+	});
+
 });
 
 var server = app.listen(5819, function() {
